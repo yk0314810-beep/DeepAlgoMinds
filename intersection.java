166621697f5e167c7e43be20b3ec500967d04602
yatnesh khandal <yk0314810@gmail.com>
2026-06-02 10:53:10 +0530

    Add files via upload
    https://leetcode.com/problems/intersection-of-two-arrays/

---
+++ intersection.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int index = 0;
+
+        for (int num : resultSet) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+}
